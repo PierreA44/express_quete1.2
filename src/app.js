9b9a067657fb26d0a768f5d51792fbb5beb92563
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2023-11-29 14:57:34 +0100

    finish quest with manual method

--- src/app.js
+++ src/app.js
@@ -7,19 +7,21 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
+const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUsers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
-app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
 
-app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
 
-app.post("/api/users", userControllers.postUser);
+app.post("/api/users", validateUser, userControllers.postUser);
 
-app.put("/api/users/:id", userControllers.updateUser);
+app.put("/api/users/:id", validateUser, userControllers.updateUser);
 
 module.exports = app;
