edf44ff95c78e5551f745c8c31b71efb4432232c
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2023-11-29 15:50:25 +0100

    maj validate

--- src/middlewares/validateMovie.js
+++ src/middlewares/validateMovie.js
@@ -12,12 +12,27 @@ const validateMovie = (req, res, next) => {
   }
   if (director == null) {
     errors.push({ field: "director", message: "This field is required" });
+  } else if (director.length >= 255) {
+    errors.push({
+      field: "director",
+      message: "Should contain less than 255 characters",
+    });
   }
   if (year == null) {
     errors.push({ field: "year", message: "This field is required" });
+  } else if (year.length >= 255) {
+    errors.push({
+      field: "year",
+      message: "Should contain less than 255 characters",
+    });
   }
   if (color == null) {
     errors.push({ field: "color", message: "This field is required" });
+  } else if (color.length >= 255) {
+    errors.push({
+      field: "color",
+      message: "Should contain less than 255 characters",
+    });
   }
   if (duration == null) {
     errors.push({ field: "duration", message: "This field is required" });
--- src/middlewares/validateUsers.js
+++ src/middlewares/validateUsers.js
@@ -4,12 +4,27 @@ const validateUser = (req, res, next) => {
 
   if (firstname == null) {
     errors.push({ field: "firstname", message: "This field is required" });
+  } else if (firstname.length >= 255) {
+    errors.push({
+      field: "firstname",
+      message: "Should contain less than 255 characters",
+    });
   }
   if (lastname == null) {
     errors.push({ field: "lastname", message: "This field is required" });
+  } else if (lastname.length >= 255) {
+    errors.push({
+      field: "lastname",
+      message: "Should contain less than 255 characters",
+    });
   }
   if (email == null) {
     errors.push({ field: "email", message: "This field is required" });
+  } else if (email.length >= 255) {
+    errors.push({
+      field: "email",
+      message: "Should contain less than 255 characters",
+    });
   }
 
   const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
@@ -19,9 +34,19 @@ const validateUser = (req, res, next) => {
   }
   if (city == null) {
     errors.push({ field: "city", message: "This field is required" });
+  } else if (city.length >= 255) {
+    errors.push({
+      field: "city",
+      message: "Should contain less than 255 characters",
+    });
   }
   if (language == null) {
     errors.push({ field: "language", message: "This field is required" });
+  } else if (language.length >= 255) {
+    errors.push({
+      field: "language",
+      message: "Should contain less than 255 characters",
+    });
   }
 
   if (errors.length) {
